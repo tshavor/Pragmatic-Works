0ffb1b9322d9a71fa1bbf8d959e737d1bf2aea07
tshavor <thomasshavor@gmail.com>
2017-01-23 08:34:19 -0600

    completed module 6, added 7

--- Intro to T-SQL/Module6_queries.sql
+++ Intro to T-SQL/Module6_queries.sql
@@ -28,6 +28,42 @@ BETWEEN
 	WHERE LastName BETWEEN 'A' AND 'C';
 	(BETWEEN works on numbers as well as string expressions!)
 
-LIKE AND WILDCARDS:
+LIKE AND WILDCARDS (%, _):
 
-5. 
+5. SELECT LastName, FirstName
+	FROM Person.Person
+	WHERE LastName LIKE 'S%';
+	(gives every person beginning with the last name of "S")
+
+6. SELECT LastName, FirstName
+	FROM Person.Person
+	WHERE LastName LIKE '%S';
+	(gives every person ending with the last name of "S")
+
+7. SELECT LastName, FirstName
+	FROM Person.Person
+	WHERE LastName LIKE '%S%';
+	(returns a LastName containing an "s" anywhere in the string)
+
+8. SELECT LastName, FirstName
+	FROM Person.Person
+	WHERE LastName LIKE 'Anders[eo]n';
+	(variations of the letters within the array brackets [] are searched for in conjunction with the name)
+
+IN Statement (similar to [])
+
+9. SELECT LastName, FirstName
+	FROM Person.Person
+	WHERE LastName IN ('Smith', 'Anderson');
+	(gives all people with the last name of Smith or Anderson)
+
+10. SELECT OrderDate, SalesOrderID
+	FROM Sales.SalesOrderHeader
+	WHERE OrderDate IN ('2011-05-31');
+	(date string is implicity created by SQL Server and searched)
+
+11. SELECT CustomerID, OrderDate, SalesOrderID
+	FROM Sales.SalesOrderHeader
+	WHERE CustomerID IN (11000, 11001);
+	(returns only customerIDs in parentheses)
+
